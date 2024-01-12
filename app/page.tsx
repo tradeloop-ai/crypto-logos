5e4b5b42f6241d6c5b1164fd160fa72a88c0e33a
Legend <ajayvignesh01@gmail.com>
2024-01-12 17:00:54 -0500

    enable router cache to enable ppr

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home({
     <div className='p-2 lg:p-3 xl:p-4'>
       <Search />
       <ul
-        key={Math.random()} // invalidate router cache
+        // key={Math.random()} // invalidate router cache
         role='list'
         className='grid grid-cols-2 justify-items-center gap-4 min-[480px]:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7'
       >
