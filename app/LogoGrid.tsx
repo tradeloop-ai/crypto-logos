f0cab24cd580a66e75e88e86530fb5ba33413a82
Legend <ajayvignesh01@gmail.com>
2024-01-12 01:24:03 -0500

    <Image unoptimized />

--- app/LogoGrid.tsx
+++ app/LogoGrid.tsx
@@ -51,6 +51,7 @@ export default async function LogoGrid({
                     width={50}
                     height={50}
                     alt=''
+                    unoptimized
                   />
                 </div>
               </div>
