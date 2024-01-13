b22dfd9bcb88486ad451a64c8e90afff0e487328
Legend <ajayvignesh01@gmail.com>
2024-01-13 17:36:16 -0500

    feat: StaticVirtualGridFallback 3

--- app/static/StaticVirtualGridFallback.tsx
+++ app/static/StaticVirtualGridFallback.tsx
@@ -7,7 +7,7 @@ export function StaticVirtualGridFallback() {
         role='list'
         className='grid w-[302px] grid-cols-2 justify-items-center gap-5 min-[480px]:w-[445px] min-[480px]:grid-cols-3 sm:w-[588px] sm:grid-cols-4 md:w-[739px] md:grid-cols-5 lg:w-[890px] lg:grid-cols-6 xl:w-[1176px] xl:grid-cols-8'
       >
-        <CryptoCardFallback count={32} className='px-[10px]' />
+        <CryptoCardFallback count={32} />
       </ul>
     </div>
   )
