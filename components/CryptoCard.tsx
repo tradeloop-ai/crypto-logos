c360c730a7a19499ebe76f9102c6f6bbb201e530
Legend <ajayvignesh01@gmail.com>
2024-01-14 11:45:38 -0500

    fix: download logo

--- components/CryptoCard.tsx
+++ components/CryptoCard.tsx
@@ -1,27 +1,71 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger
+} from '@/components/ui/context-menu'
 import { CMCMap } from '@/lib/types'
+import { DownloadIcon } from '@radix-ui/react-icons'
 import Image from 'next/image'
 
 export function CryptoCard({ crypto }: { crypto: CMCMap }) {
+  const handleDownload = async () => {
+    try {
+      const response = await fetch(
+        `https://s2.coinmarketcap.com/static/img/coins/128x128/${crypto?.id}.png`
+      )
+      const blob = await response.blob()
+      const url = window.URL.createObjectURL(blob)
+      const link = document.createElement('a')
+      link.href = url
+      link.download = `${crypto.symbol}.png`
+      document.body.appendChild(link)
+      link.click()
+      window.URL.revokeObjectURL(url)
+      document.body.removeChild(link)
+    } catch (error) {
+      console.error('Error downloading the image:', error)
+    }
+  }
+
   return (
-    <div className='flex h-[160px] w-[135px] flex-col rounded-md border border-accent bg-white py-2 hover:border-muted-foreground dark:bg-black'>
-      <div className='flex basis-8/12 items-center justify-center'>
-        <div>
-          <Image
-            className='rounded-full border'
-            src={`https://s2.coinmarketcap.com/static/img/coins/128x128/${crypto?.id}.png`}
-            width={50}
-            height={50}
-            alt=''
-            unoptimized
-          />
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <div className='relative flex h-[160px] w-[135px] flex-col rounded-md border border-accent bg-white py-2 hover:border-muted-foreground dark:bg-black'>
+          {/*<Button*/}
+          {/*  variant='outline'*/}
+          {/*  size='icon'*/}
+          {/*  className='absolute right-2 top-2 h-5 w-5'*/}
+          {/*  onClick={handleDownload}*/}
+          {/*>*/}
+          {/*  <DownloadIcon className='h-3 w-3' />*/}
+          {/*</Button>*/}
+          <div className='flex basis-8/12 items-center justify-center'>
+            <div>
+              <Image
+                className='rounded-full border'
+                src={`https://s2.coinmarketcap.com/static/img/coins/128x128/${crypto?.id}.png`}
+                width={50}
+                height={50}
+                alt=''
+                unoptimized
+              />
+            </div>
+          </div>
+          <div className='flex basis-4/12 items-center justify-center'>
+            <div className='text-center'>
+              <p className='font-semibold'>{crypto.symbol}</p>
+              <p className='text-sm text-muted-foreground'>{crypto.name}</p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className='flex basis-4/12 items-center justify-center'>
-        <div className='text-center'>
-          <p className='font-semibold'>{crypto.symbol}</p>
-          <p className='text-sm text-muted-foreground'>{crypto.name}</p>
-        </div>
-      </div>
-    </div>
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem onClick={handleDownload}>Download Logo</ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   )
 }
