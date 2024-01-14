0c3d29306d40949e9123d0f0bf6997d2b0817420
Legend <ajayvignesh01@gmail.com>
2024-01-13 23:59:33 -0500

    Revert "static search suspense try 3"
    This reverts commit 1b937bfdff2fd8030a7f5292761fc1ba1ccb3131.

--- app/static/StaticSearchFallback.tsx
+++ app/static/StaticSearchFallback.tsx
@@ -1,8 +1,9 @@
 import { Skeleton } from '@/components/ui/skeleton'
+import { cn } from '@/lib/utils'
 
 export function StaticSearchFallback({ className }: { className?: string }) {
   return (
-    <div className={'flex w-full items-center justify-center pb-4 ' + className}>
+    <div className={cn('flex w-full items-center justify-center pb-4', className)}>
       <Skeleton className='h-9 w-full max-w-sm' />
     </div>
   )
