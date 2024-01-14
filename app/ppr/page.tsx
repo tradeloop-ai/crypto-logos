695c20d5d22df315f00dc557816106a7a99be854
Legend <ajayvignesh01@gmail.com>
2024-01-14 00:13:23 -0500

    static search suspense try 4

--- app/ppr/page.tsx
+++ app/ppr/page.tsx
@@ -1,6 +1,6 @@
 import { CMCGrid } from '@/app/ppr/CMCGrid'
+import { StaticSearchFallback } from '@/app/static/StaticSearchFallback'
 import { Search } from '@/components/Search'
-import { SearchFallback } from '@/components/SearchFallback'
 import { VirtualGridFallback } from '@/components/VirtualGridFallback'
 import { Suspense } from 'react'
 
@@ -8,7 +8,7 @@ export default function PPR() {
   return (
     <div>
       <div className='fixed z-50 h-16 w-full bg-transparent backdrop-blur-lg'>
-        <Suspense fallback={<SearchFallback className={'fixed top-4 pb-0'} />}>
+        <Suspense fallback={<StaticSearchFallback />}>
           <Search className={'fixed top-4 pb-0'} />
         </Suspense>
       </div>
--- app/static/StaticSearchFallback.tsx
+++ app/static/StaticSearchFallback.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export function StaticSearchFallback() {
   return (
-    <div className='flex w-full items-center justify-center pb-4'>
-      <Skeleton className='fixed top-4 h-9 w-full max-w-sm' />
+    <div className='fixed top-4 flex w-full items-center justify-center'>
+      <Skeleton className='h-9 w-full max-w-sm' />
     </div>
   )
 }
