1b937bfdff2fd8030a7f5292761fc1ba1ccb3131
Legend <ajayvignesh01@gmail.com>
2024-01-13 23:57:55 -0500

    static search suspense try 3

--- app/static/StaticSearchFallback.tsx
+++ app/static/StaticSearchFallback.tsx
@@ -1,9 +1,8 @@
 import { Skeleton } from '@/components/ui/skeleton'
-import { cn } from '@/lib/utils'
 
 export function StaticSearchFallback({ className }: { className?: string }) {
   return (
-    <div className={cn('flex w-full items-center justify-center pb-4', className)}>
+    <div className={'flex w-full items-center justify-center pb-4 ' + className}>
       <Skeleton className='h-9 w-full max-w-sm' />
     </div>
   )
