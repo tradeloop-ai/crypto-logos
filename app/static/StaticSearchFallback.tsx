344c738e060e00389657bc571c86f8e3a218fd6b
Legend <ajayvignesh01@gmail.com>
2024-01-14 00:28:41 -0500

    static search suspense try 6

--- app/static/StaticSearchFallback.tsx
+++ app/static/StaticSearchFallback.tsx
@@ -1,14 +1,9 @@
-import { Input } from '@/components/ui/input'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export function StaticSearchFallback() {
   return (
     <div className='flex w-full items-center justify-center pb-4'>
-      <Input
-        type='text'
-        id='loading'
-        placeholder='Loading...'
-        className='fixed top-4 w-full max-w-sm'
-      />
+      <Skeleton className='fixed top-4 w-full max-w-sm' />
     </div>
   )
 }
