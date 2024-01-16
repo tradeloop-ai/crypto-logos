14afaf6c0c742e76a4fb20a5c7e4fa9236435201
Legend <ajayvignesh01@gmail.com>
2024-01-16 00:20:03 -0500

    feat: cron jobs 2

--- app/api/cron/cmc/route.ts
+++ app/api/cron/cmc/route.ts
@@ -1,13 +1,14 @@
 import { CMCMap } from '@/lib/types'
 import { sql } from '@vercel/postgres'
-import { NextResponse, type NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
-export async function GET(request: NextRequest) {
+export const dynamic = 'force-dynamic'
+
+export async function GET() {
   const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
     headers: {
       'X-CMC_PRO_API_KEY': process.env.CMC_PRO_API_KEY! as string
-    },
-    cache: 'no-store'
+    }
   })
   const json = await response.json()
   const cmc = json.data as CMCMap[]
@@ -19,6 +20,10 @@ export async function GET(request: NextRequest) {
      SELECT id, rank, name, symbol, slug, is_active, first_historical_data, last_historical_data, platform::jsonb
      FROM json_populate_recordset(NULL::public.cmc, $1)`,
     [JSON.stringify(cmc)]
+  )
+
+  console.log(
+    `${delete_query.rowCount} coins deleted & ${insert_query.rowCount} coins inserted from CMC`
   )
 
   return NextResponse.json(
